12cab494933aefa55a4a0bcd5c1f7b0cada2e852
Finn Kranzosch <s_kranzosch18@stud.hwr-berlin.de>
2020-06-04 15:50:03 +0200

    Implemented representation and creation of lists

--- scripts/board.js
+++ scripts/board.js
@@ -1,9 +1,9 @@
+getLists();
+
 function getLists(){
     var content = document.getElementById("board_content");
     content.innerHTML = LOADER_HTML;
     var reqUrl = url + "/list/" + getParams(window.location.href).board_id;
-    console.log(reqUrl);
-    
     var req = new XMLHttpRequest();
     req.overrideMimeType("application/json");
     req.open("GET", reqUrl, true);
@@ -11,10 +11,12 @@ function getLists(){
     req.onreadystatechange = function() {
         if (this.readyState == 4) {
             if (this.status == 200) {
-                var jsonResponse = JSON.parse(req.responseText);
+                var jsonResponse = JSON.parse(req.responseText);                
                 content.innerHTML = parseListsToHtml(jsonResponse);
             } else if (this.status == 401) {
                 window.location = UNAUTHORIZED_URL;
+            } else if (this.status == 404) {
+                content.innerHTML = "<h1>Dieses Board existiert nicht</h1>";
             } else {
                 console.log(this.status);
                 console.log(this.responseText);
@@ -22,4 +24,76 @@ function getLists(){
         }
     }
     req.send();
+}
+
+function createListRequest(board_id){
+    var list_title = document.getElementById("create-list-input").value;
+    var reqUrl = url + "/list";
+    var req = new XMLHttpRequest();
+    req.overrideMimeType("application/json");
+    req.open("POST", reqUrl, true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.setRequestHeader("Authorization", getCookieByName("token"));
+    req.onreadystatechange = function() {
+        if (this.readyState == 4){
+            if (this.status == 201) {
+                // created
+                console.log("created");
+                getLists();
+            } else if (this.status == 401) {
+                window.location = UNAUTHORIZED_URL;
+            } else if (this.status == 404) {
+                // not found
+                console.log("not found");
+            } else if (this.status == 422) {
+                // duplicate
+                console.log("duplicate");
+            } else {
+                console.log(this.status);
+                console.log(this.responseText);
+            }
+        }
+    }
+    const list = JSON.stringify({
+        "title": list_title,
+        "board_id": board_id
+    });
+    console.log(list);
+    req.send(list);
+}
+
+function createList(board_id, elem) {
+    var container = elem.parentElement;
+    container.innerHTML = createListInputHtml(board_id);
+    console.log(createListInputHtml(board_id));
+}
+
+function parseListsToHtml(res){
+    var content_html = ""
+    res.forEach(function(list, index, array){
+        if (!(index == array.length -1)){
+            content_html += createListHtml(list.id, list.title);
+        } else {
+            // last element
+            document.getElementById("board-title").innerHTML = `${list.board_title}`;
+            content_html = `<div id="lists_${list.board_id}" class="board-lists">` + content_html + createListBtnHtml(list.board_id) + "</div>";
+        }
+    });
+    return content_html;
+}
+
+function createListBtnHtml(board_id){
+    return `<div id="create-list"><div id="create-list-btn" onclick="createList(${board_id}, this)"><b>+</b>Liste hinzufügen</div></div>`;
+}
+
+function createListHtml(list_id, list_title){
+    return `<div id="list_${list_id}" class="list-element">${list_title}</div>`;
+}
+
+function createListInputHtml(board_id){
+    return `<div id="create-list-form"><input type="text" id="create-list-input" placeholder="Titel hinzufügen"><button id="create-list-submit" onclick="createListRequest(${board_id})"><b>+</b></button></div>`;
+}
+
+function showDashboard(){
+    window.location = DASHBOARD_URL;
 }
